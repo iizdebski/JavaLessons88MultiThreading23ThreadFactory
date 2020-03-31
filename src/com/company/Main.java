445bed06b285d3ca9658cf61ca5f1a72dc87482c
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-31 22:01:56 +0300

    ThreadFactory

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.concurrent.ThreadFactory;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        ThreadFactory threadFactory = new ThreadFactory() {
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setPriority(Thread.MAX_PRIORITY);
+                return thread;
+            }
+        };
+        threadFactory.newThread(new MyRun()).start();
+    }
+    static class MyRun implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(1);
+
+        }
+    }
+}
